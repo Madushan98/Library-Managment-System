ff0f2d001800649d1f7098f4266b1129bc26c961
GeethCY <geethcyapa@gmail.com>
2023-04-21 19:01:44 +0530

    BookRecord

---
+++ src/main/java/Library/Entity/BookRecord.java
@@ -0,0 +1,41 @@
+package Library.Entity;
+
+import java.time.LocalDate;
+
+public class BookRecord {
+
+    private  boolean overdue;
+
+    private int bookId;
+
+    private LocalDate dueDate;
+
+    private String user;
+
+    public BookRecord(){
+        overdue = false;
+    }
+    public boolean isOverdue() {
+        return overdue;
+    }
+
+    public void setOverdue(boolean overdue) {
+        this.overdue = overdue;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
