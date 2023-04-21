83d77cd5c1fb03e092a1edf02f2ee7a45f04ae6a
GeethCY <geethcyapa@gmail.com>
2023-04-21 19:43:45 +0530

    improved-constructor

--- src/main/java/Library/Entity/BookRecord.java
+++ src/main/java/Library/Entity/BookRecord.java
@@ -4,15 +4,15 @@ import java.time.LocalDate;
 
 public class BookRecord {
     private boolean overdue;
-
     private int bookId;
-
     private LocalDate dueDate;
-
     private String user;
 
-    public BookRecord() {
-        overdue = false;
+    public BookRecord(int bookId, LocalDate dueDate, boolean overdue, String user ) {
+        this.bookId = bookId;
+        this.dueDate = dueDate;
+        this.overdue = overdue;
+        this.user = user;
     }
 
     public boolean isOverdue() {
