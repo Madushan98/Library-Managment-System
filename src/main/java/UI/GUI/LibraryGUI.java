eb5f83993c5306055cb2d90e6242ebd80751849d
ManojBD <manojdissanayake8888@gmail.com>
2023-04-22 10:04:47 +0530

    added borrowBook, showOverdueBooks

--- src/main/java/UI/GUI/LibraryGUI.java
+++ src/main/java/UI/GUI/LibraryGUI.java
@@ -7,6 +7,11 @@ import UI.UI;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.List;
 
 public class LibraryGUI implements UI {
@@ -50,6 +55,12 @@ public class LibraryGUI implements UI {
         JButton removeButton = new JButton("Remove Book");
         removeButton.addActionListener(e -> removeBook());
         controlPanel.add(removeButton);
+        JButton borrowButton = new JButton("Borrow Book");
+        borrowButton.addActionListener(e -> borrowBook());
+        controlPanel.add(borrowButton);
+        JButton showOverDueBooksButton = new JButton("Overdue Books");
+        showOverDueBooksButton.addActionListener(e -> showOverdueBooks());
+        controlPanel.add(showOverDueBooksButton);
         JButton refreshButton = new JButton("Refresh");
         refreshButton.addActionListener(e -> refreshTable());
         controlPanel.add(refreshButton);
@@ -106,4 +117,70 @@ public class LibraryGUI implements UI {
         }
     }
 
+    private void borrowBook() {
+        // get the selected book from the table
+        int selectedRow = table.getSelectedRow();
+        if (selectedRow == -1) {
+            JOptionPane.showMessageDialog(frame, "Please select a book to borrow.");
+            return;
+        }
+        int bookId = (int) table.getValueAt(selectedRow, 0);
+        JTextField userField = new JTextField();
+        JSpinner dateField = new JSpinner(new SpinnerDateModel());
+        Object[] inputFields = {
+                "User:", userField,
+                "Borrow Date:", dateField
+        };
+        int result = JOptionPane.showConfirmDialog(frame, inputFields, "Borrow Book", JOptionPane.OK_CANCEL_OPTION);
+
+        if (result == JOptionPane.OK_OPTION) {
+            String borrower = userField.getText();
+            Date borrowDate = ((SpinnerDateModel) dateField.getModel()).getDate();
+            Instant instant = borrowDate.toInstant();
+            ZoneId zoneId = ZoneId.systemDefault();
+            ZonedDateTime zonedDateTime = instant.atZone(zoneId);
+            LocalDate localDate = zonedDateTime.toLocalDate();
+
+            // borrow the book
+            Book bookBorrowed = libraryService.borrowBook(bookId, borrower, localDate);
+
+            if (bookBorrowed != null) {
+                JOptionPane.showMessageDialog(frame, "Book borrowed successfully!");
+                refreshTable();
+            } else {
+                JOptionPane.showMessageDialog(frame, "Sorry, this book is not available for borrowing.");
+            }
+        }
+    }
+
+    private void showOverdueBooks() {
+        JDialog dialog = new JDialog(frame, "Overdue Books", true);
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("ID");
+        model.addColumn("Title");
+        model.addColumn("Author");
+        model.addColumn("Borrowed User");
+        model.addColumn("Borrowed Date");
+        model.addColumn("Due Date");
+
+        JTable table = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        List<Book> overdueBooks = libraryService.getOverdueBooks();
+
+//        for (Book book : overdueBooks) {
+//            record = libraryService.getBookRecord()
+//            LocalDate dueDate = book.getDueDate();
+//            String borrowedUser = book.getBorrowedUser();
+//            String borrowedDate = book.getBorrowedDate().toString();
+//            model.addRow(new Object[] { book.getId(), book.getTitle(), book.getAuthor(), borrowedUser, borrowedDate, dueDate });
+//        }
+
+        dialog.add(scrollPane);
+        dialog.pack();
+        dialog.setLocationRelativeTo(frame);
+        dialog.setVisible(true);
+    }
 }
