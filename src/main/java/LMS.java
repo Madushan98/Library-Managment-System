5a333f39de1e46bfd2c3f5d6c271faf8ca10b30d
Charuka Samarakoon <charuka@protonmail.com>
2023-04-22 08:01:30 +0530

    Remove unused imports

--- src/main/java/LMS.java
+++ src/main/java/LMS.java
@@ -1,11 +1,7 @@
-import java.util.List;
-
+import Enums.SupportSqlDB;
 import Enums.UIType;
 import Library.Database.Adapters.Sql;
-import Enums.SupportSqlDB;
 import Library.Database.Interfaces.DataHandler;
-import Library.Entity.Book;
-import Library.Entity.BookFactory;
 import Library.Service.BookLibraryService;
 import Library.Service.LibraryService;
 import UI.UI;
@@ -15,13 +11,6 @@ public class LMS {
 
     public static void main(String[] args) {
         DataHandler dataHandler = new Sql(SupportSqlDB.SQLITE);
-
-        System.out.println("Create books");
-
-//        dataHandler.CreateBook(BookFactory.createBook("The Lord of the Rings", "J.R.R. Tolkien"));
-//        dataHandler.CreateBook(BookFactory.createBook("The Hobbit", "J.R.R. Tolkien"));
-//        dataHandler.CreateBook(BookFactory.createBook("The Silmarillion", "J.R.R. Tolkien"));
-//        dataHandler.CreateBook(BookFactory.createBook("The Fellowship of the Ring", "J.R.R. Tolkien"));
 
         LibraryService bookLibraryService = new BookLibraryService(dataHandler);
 
